85fdf9b76f4a2088da7b2c0d6599fd2f1dbe0920
brysona10 <addisonbryson10@gmail.com>
2018-08-01 13:18:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
-
-
+	get '/hello'do
+	  erb :hello
+	end
+	get '/goodbye'do 
+	  "Goodbye Joe"
+	  erb :goodbye 
+	end
+	get '/date' do
+	  erb :date
+	end
 end
